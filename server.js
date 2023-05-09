cadeb6c9dde89d8066c30931bb7b11fd7bf86647
Sebasttian Mucci <mucci310@gmail.com>
2023-05-09 20:14:43 -0300

    perdido

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ let PORT = 8080
 let ready = () => console.log('server ready on port:' + PORT);
 
 server.listen(PORT, ready)
+server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
 let index_route = '/'
@@ -38,23 +39,78 @@ let one_function = (req, res) => {
 server.get(one_route, one_function)
 
 let query_route = '/products'
-let query_function = (req, res)=>{
+let query_function = (req, res) => {
     console.log(req.query);
     let quantity = req.query.quantity ?? 5
-    let products = manager.get_products().slice(0,quantity) //array de productos que tengo que rebanar para que pagine segun la query qe envia el cliente
-    if(products.lenght>0){
+    let products = manager.get_products().slice(0, quantity) //array de productos que tengo que rebanar para que pagine segun la query qe envia el cliente
+    if (products.lenght > 0) {
         return res.send({
             succes: true,
             products: products
         })
-    }else{
+    } else {
         return res.send({
             succes: false,
             products: 'inexistente'
         })
     }
-    
+
 }
 server.get(query_route, query_function)
 
 //products: 'inexistente'
+
+server.post(
+    '/products',
+    async (req, res) => {
+        try {
+            let name = req.body.name ?? null
+            let description = req.body.description ?? null
+            let price = req.body.price ?? null
+            let stock = req.body.stock ?? null
+            let image = req.body.image ?? null
+            if (name && description && price && stock && image) {
+                let product = await manager.add_product({ name, description, price, stock, image })
+                return res.json({
+                    status: 201,
+                    product_id: product.id,
+                    message: 'Created'
+                })
+            } else {
+                return res.json({
+                    status: 400,
+                    message: 'Check params!'
+                })
+            }
+
+        } catch (error) {
+            console.log(error);
+            return res.json({
+                status: 500,
+                message:'error'
+            })
+        }
+    }
+)
+
+server.put(
+    '/products/:pid',
+    (req, res) => {
+        if (req.body&&req.params.pid){
+            let id = Number (req.params.pid)
+            let data = req.body
+            manager.update_product(id, data) 
+            return res.json({
+                status: 200,
+                message: 'updated!'
+            })
+        }else{
+            return res.json({
+                status: 400,
+                message: 'check data!'
+            })
+        }    
+    }
+    //en este caso no necesito esperar la actaulizacion, debido a que los datos de la actualizacion no se utilizan para operar o enviar al cliente en la respuesta. por eso no necesito una async
+)
+
